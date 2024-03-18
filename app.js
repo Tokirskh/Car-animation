26c2d337cd88711d640956ee0926adc5d780005b
Tokir Sheikh <tokirs84@gmail.com>
2024-03-18 22:52:37 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// var audio = document.createElement("audio");
+// audio.setAttribute("src", "sound.mp3");
+// audio.loop = true;
+// audio.play();
+// 
+ 
+   const btn = document.querySelector("button");
+   btn.addEventListener("click", () =>{
+    btn.style.backgroundColor = prompt("enter the color");
+   } )
